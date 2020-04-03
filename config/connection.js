3899afbb83e490103cef4b00a46dc03aa2615a4c
Rachel Humble <rachelhumble@me.com>
2020-04-03 01:23:37 -0400

    heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,25 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "mysql123",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "mysql123",
+//   database: "burgers_db"
+// });
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "mysql123",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
